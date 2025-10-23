c86a63e1c71bf2d3ddd675378d63ca3759862f9b
lyb5382 <lyb538202@gmail.com>
2025-10-23 19:51:50 +0900

    df

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -16,6 +16,7 @@ const authenticateToken = (req, res, next) => {
     if (!token) return res.status(401).json({ message: '토큰 없음' })
     try {
         req.user = jwt.verify(token, process.env.JWT_SECRET)
+        next()
     } catch (error) {
         return res.status(403).json({ message: '유효하지 않은 토큰' })
     }
